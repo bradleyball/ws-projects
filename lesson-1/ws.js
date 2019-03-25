691a3810f9878e1cc02db724012b8c1f549b73f6
cacheUp <bradb26@gmail.com>
2019-03-25 17:20:37 +0300

    finsihed setting up vanilla web socket

--- lesson-1/ws.js
+++ lesson-1/ws.js
@@ -1,15 +1,24 @@
-const http = require('http')
+const http = require("http");
 
 // 3rd party module, ws
-const websocket = require('ws')
+const websocket = require("ws");
 
-const server = http.createServer((req,res) => {
-    res.end("I am connected")
+const server = http.createServer((req, res) => {
+  res.end("I am connected");
+});
 
-})
+const wss = new websocket.Server({ server });
+wss.on("headers", (headers, req) => {
+  console.log(headers);
+});
 
-
+wss.on("connection", (ws, req) => {
+  ws.send("welcome to web sockets!!");
+  ws.on("message", msg => {
+    console.log(msg);
+  });
+});
 
 server.listen(8000, () => {
-    console.log("listening at 8000")
-})
+  console.log("listening at 8000");
+});
