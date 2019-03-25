e4de278817b97a21fbf9450b5f7047ebece2b177
cacheUp <bradb26@gmail.com>
2019-03-25 22:10:48 +0300

    basic socket io

---
+++ socketio-101/basic.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const socketio = require("socket.io");
+
+// 3rd party module, ws
+
+const server = http.createServer((req, res) => {
+  res.end("I am connected");
+});
+
+const io = socketio(server);
+
+io.on("connection", (socket, req) => {
+  socket.emit("welcome", "welcome to web sockets!!");
+  socket.on("message", msg => {
+    console.log(msg);
+  });
+});
+
+server.listen(8001, () => {
+  console.log("listening at 8001");
+});
