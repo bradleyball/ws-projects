955969d77e8f98ab0477f4d33cf7c54c20f08f8b
cacheUp <bradb26@gmail.com>
2019-04-02 21:53:07 +0300

    cleaned up scripts.js

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,20 +1,22 @@
 const socket = io("http://localhost:9000");
-const socket2 = io("http://localhost:9000/wiki");
-const socket3 = io("http://localhost:9000/mozilla");
-const socket4 = io("http://localhost:9000/linux");
 
+console.log(socket.io);
 socket.on("connect", () => {
   console.log(socket.id);
 });
 
-socket2.on("connect", () => {
-  console.log(socket.id);
+socket.on("messageFromServer", dataFromServer => {
+  console.log(dataFromServer);
+  socket.emit("messageToServer", { data: "Data from client" });
 });
 
-socket.on("welcome", msg => {
-  console.log(msg);
+document.querySelector("#message-form").addEventListener("submit", event => {
+  event.preventDefault();
+  const newMessage = document.querySelector("#user-message").value;
+  socket.emit("newMessageToServer", { text: newMessage });
 });
 
-socket2.on("welcome", msg => {
+socket.on("messageToClients", msg => {
   console.log(msg);
+  document.querySelector("#messages").innerHTML += `<li>${msg.text}</li>`;
 });
