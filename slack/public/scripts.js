b63b986b8c267721eb5cf85f8713410cdb0df1ec
cacheUp <bradb26@gmail.com>
2019-04-02 21:42:58 +0300

    added Namespace, Rooms, modified scripts

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,21 +1,20 @@
 const socket = io("http://localhost:9000");
-const socket2 = io("http://localhost:9000/admin"); // This is the / endpoint
+const socket2 = io("http://localhost:9000/wiki");
+const socket3 = io("http://localhost:9000/mozilla");
+const socket4 = io("http://localhost:9000/linux");
 
-socket.on("messageFromServer", dataFromServer => {
-  console.log(dataFromServer);
-  socket.emit("messageToServer", { data: "Data from client" });
+socket.on("connect", () => {
+  console.log(socket.id);
 });
 
-socket.on("joined", msg => {
+socket2.on("connect", () => {
+  console.log(socket.id);
+});
+
+socket2.on("welcome", msg => {
   console.log(msg);
 });
 
-socket2.on("welcome", dataFromServer => {
-  console.log(dataFromServer);
-});
-
-document.querySelector("#message-form").addEventListener("submit", event => {
-  event.preventDefault();
-  const newMessage = document.querySelector("#user-message").value;
-  socket.emit("newMessageToServer", { text: newMessage });
+socket2.on("welcome", msg => {
+  console.log(msg);
 });
--- slack/slack.js
+++ slack/slack.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const socketio = require("socket.io");
+let namespaces = require("./data/namespaces");
 
 app.use(express.static(__dirname + "/public"));
 
@@ -9,6 +10,13 @@ const expressServer = app.listen(9000, () => {
 });
 
 const io = socketio(expressServer);
+
+//loop trough each namespace and listen for a connection
+namespaces.forEach(namespace => {
+  io.of(namespace.endpoint).on("connection", socket => {
+    console.log(`${socket.id} has joined ${namespace.endpoint}`);
+  });
+});
 
 io.on("connection", socket => {
   socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
