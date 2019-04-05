5ca3f7be7de805804820bb441cbf1192c8ca1263
cacheUp <bradb26@gmail.com>
2019-04-05 23:45:39 +0300

    abstracted files for readability

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,10 +1,5 @@
 const socket = io("http://localhost:9000");
 
-console.log(socket.io);
-socket.on("connect", () => {
-  console.log(socket.id);
-});
-
 socket.on("nsList", nsData => {
   console.log("The list of namespaces has arrived");
   console.log(nsData);
@@ -20,23 +15,6 @@ socket.on("nsList", nsData => {
     i.addEventListener("click", e => {
       const nsEndpoint = i.getAttribute("ns");
       console.log(`${nsEndpoint} I should go to now`);
-    });
-  });
-  const nsSocket = io("http://localhost:9000/wiki");
-  nsSocket.on("nsRoomLoad", nsRooms => {
-    let roomList = document.querySelector(".room-list");
-    roomList.innerHTML = "";
-    nsRooms.forEach(room => {
-      let glyph;
-      room.privateRoom ? (glyph = "lock") : (glyph = "globe");
-      roomList.innerHTML += ` <li class="room"><span class="glyphicon glyphicon-${glyph}">
-                              </span>${room.roomTitle}</li>`;
-    });
-    let roomNodes = document.getElementsByClassName("room");
-    Array.from(roomNodes).forEach(elem => {
-      elem.addEventListener("click", e => {
-        console.log("Someone clicked on", e.target.innerHTML);
-      });
     });
   });
 });
