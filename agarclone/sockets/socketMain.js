bd9713cd645bbea1fa8db2d641a191cd1451b0ae
cacheUp <bradb26@gmail.com>
2019-04-15 03:35:45 +0300

    emit created orbs back to client (socket.io)

--- agarclone/sockets/socketMain.js
+++ agarclone/sockets/socketMain.js
@@ -2,6 +2,13 @@ const io = require("../server").io;
 const Orb = require("./classes/Orb");
 
 let orbs = [];
+initGame();
+
+io.sockets.on("connect", socket => {
+  socket.emit("init", {
+    orbs
+  });
+});
 
 const initGame = () => {
   for (let i = 0; i < 500; i++) {
