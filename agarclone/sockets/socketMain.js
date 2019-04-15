848810f15a9b92e742ec6f4207c55603e87e482d
cacheUp <bradb26@gmail.com>
2019-04-15 03:33:49 +0300

    loop through world orbs to make 50 new orbs

--- agarclone/sockets/socketMain.js
+++ agarclone/sockets/socketMain.js
@@ -1,3 +1,12 @@
 const io = require("../server").io;
+const Orb = require("./classes/Orb");
+
+let orbs = [];
+
+const initGame = () => {
+  for (let i = 0; i < 500; i++) {
+    orbs.push(new Orb());
+  }
+};
 
 module.exports = io;
